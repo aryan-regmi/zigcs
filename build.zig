9cb17ba5af3387013610606b1b6d3e427e29b596
Aryan Regmi <arre9260@protonmail.com>
2023-05-22 17:34:14 -0600

    Changed main.zig to lib.zig

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "Zigcs",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/lib.zig" },
         .target = target,
         .optimize = optimize,
     });
@@ -17,14 +17,14 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/lib.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    const run_main_tests = b.addRunArtifact(main_tests);
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
