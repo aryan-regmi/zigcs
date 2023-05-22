2c974cba8a1856adb1e719bd9bd898fcad9399d3
Aryan Regmi <arre9260@protonmail.com>
2023-05-22 16:57:01 -0600

    Added mutex to `World`

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,14 @@ const testing = std.testing;
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 
-const System = *const fn (ctx: *Context) anyerror!void;
+const System = *const fn (ctx: Context) anyerror!void;
 
 const Entity = usize;
 
 const World = struct {
     const Self = @This();
 
-    num_entities: usize,
+    num_entities: usize = 0,
 
     fn spawnEntity(self: *Self) Entity {
         const entity = self.num_entities;
@@ -22,7 +22,11 @@ const World = struct {
 const EntityBuilder = struct {
     const Self = @This();
 
-    ctx: *Context,
+    ctx: Context,
+
+    pub fn init(ctx: Context) Self {
+        return Self{ .ctx = ctx };
+    }
 
     // TODO: Implement
     pub fn with(self: Self, component: anytype) Self {
@@ -32,7 +36,9 @@ const EntityBuilder = struct {
 
     // TODO: Implement
     pub fn build(self: Self) Entity {
+        self.ctx.world_mutex.lock();
         var entity = self.ctx.world.*.spawnEntity();
+        self.ctx.world_mutex.unlock();
 
         return entity;
     }
@@ -42,13 +48,14 @@ const Context = struct {
     const Self = @This();
 
     world: *World,
+    world_mutex: *std.Thread.Mutex,
 
-    pub fn init(world: *World) Self {
-        return Self{ .world = world };
+    fn init(world: *World, mutex: *std.Thread.Mutex) Self {
+        return Self{ .world = world, .world_mutex = mutex };
     }
 
-    pub fn spawn(self: *Self) EntityBuilder {
-        return EntityBuilder{ .ctx = self };
+    pub fn spawn(self: Self) EntityBuilder {
+        return EntityBuilder.init(self);
     }
 };
 
@@ -77,19 +84,21 @@ const App = struct {
     }
 
     // TODO: Move to a scheduler struct eventually?
-    fn runSystem(ctx: *Context, system: System) !void {
+    fn runSystem(ctx: Context, system: System) !void {
         try system(ctx);
     }
 
+    /// Runs all the systems in the [App](App)
     pub fn run(self: *Self) !void {
         var threads = try std.ArrayList(std.Thread).initCapacity(self.allocator, self.systems.items.len);
         defer threads.deinit();
 
-        var ctx = Context.init(&self.world);
+        var mutex = std.Thread.Mutex{};
 
         for (self.systems.items) |system| {
-            // FIXME: Make sharing the world thread-safe (mutex?)
-            try threads.append(try std.Thread.spawn(.{}, runSystem, .{ &ctx, system }));
+            // FIXME: Make sharing the world thread-safe (mutex on world?)
+            var ctx = Context.init(&self.world, &mutex);
+            try threads.append(try std.Thread.spawn(.{}, runSystem, .{ ctx, system }));
         }
 
         for (threads.items) |thr| {
@@ -98,47 +107,58 @@ const App = struct {
     }
 };
 
+// NOTE: Change to c_allocator if running valgrind!
 test "can spawn entities" {
-    const testSystems = struct {
-        fn system1(ctx: *Context) !void {
-            var entity1 = ctx.spawn().build();
-            try testing.expectEqual(entity1, 0);
+    std.testing.log_level = .debug;
 
-            var entity2 = ctx.spawn().build();
-            try testing.expectEqual(entity2, 1);
+    // const allocator = std.heap.c_allocator;
+    const allocator = testing.allocator;
+
+    const testSystems = struct {
+        fn system1(ctx: Context) !void {
+            const e1 = ctx.spawn().build();
+            std.log.debug("[System 1] Entity {}", .{e1});
+
+            const e2 = ctx.spawn().build();
+            std.log.debug("[System 1] Entity {}", .{e2});
         }
 
-        fn system2(ctx: *Context) !void {
-            // NOTE: Sleep so system2 runs after system1
-            std.time.sleep(100);
+        fn system2(ctx: Context) !void {
+            const e1 = ctx.spawn().build();
+            std.log.debug("[System 2] Entity {}", .{e1});
 
-            var entity3 = ctx.spawn().build();
-            try testing.expectEqual(entity3, 2);
+            const e2 = ctx.spawn().build();
+            std.log.debug("[System 2] Entity {}", .{e2});
+        }
 
-            var entity4 = ctx.spawn().build();
-            try testing.expectEqual(entity4, 3);
+        fn system3(ctx: Context) !void {
+            // NOTE: Sleep so this runs last
+            std.time.sleep(1000);
+            try testing.expectEqual(ctx.world.num_entities, 4);
         }
     };
 
-    var app = App.init(testing.allocator);
+    // var app = App.init(testing.allocator);
+    var app = App.init(allocator);
     defer app.deinit();
 
     try app.addSystem(testSystems.system1);
     try app.addSystem(testSystems.system2);
+    try app.addSystem(testSystems.system3);
     try app.run();
 }
 
 test "can spawn entities with components" {
+    // const allocator = std.heap.c_allocator;
+    const allocator = testing.allocator;
+
     const Health = struct { hp: usize };
     const Age = struct { age: usize };
 
     const SpawnEntitySystem = struct {
         // TODO: Write tests to check for components!!
-        fn run(ctx: *Context) !void {
-            const NPC = ctx
-                .spawn()
-                .with(Health{ .hp = 50 })
-                .build();
+        fn run(ctx: Context) !void {
+            const NPC = ctx.spawn().with(Health{ .hp = 50 }).build();
             _ = NPC;
 
             const PLAYER = ctx
@@ -150,7 +170,8 @@ test "can spawn entities with components" {
         }
     };
 
-    var app = App.init(testing.allocator);
+    // var app = App.init(testing.allocator);
+    var app = App.init(allocator);
     defer app.deinit();
 
     try app.addSystem(SpawnEntitySystem.run);
