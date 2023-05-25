5483c48d3136af157b32e9fb635406fd174a3626
Aryan Regmi <arre9260@protonmail.com>
2023-05-25 10:10:08 -0600

    Export lib as zig module

--- build.zig
+++ build.zig
@@ -29,12 +29,14 @@ pub fn build(b: *std.Build) void {
     // running `zig build`).
     b.installArtifact(lib);
 
-    // const zigcs_module = b.createModule(.{
-    //     .source_file = .{ .path = "src/lib.zig" },
-    //     .dependencies = &.{},
-    // });
-    // _ = zigcs_module;
-    // b.addModule("zigcs", .{});
+    // Export zigcs as a module
+    b.addModule(
+        "zigcs",
+        .{
+            .source_file = .{ .path = "src/lib.zig" },
+            .dependencies = &.{},
+        },
+    );
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
